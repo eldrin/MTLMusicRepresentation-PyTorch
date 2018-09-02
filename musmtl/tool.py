3f5496c0d073deb8f1f6ced7d670b7f05658897a
Jay Kim - EWI <jaykim@insy-login.hpc.tudelft.nl>
2018-09-03 00:01:56 +0200

    updated feature extraction for better efficientcy

--- musmtl/tool.py
+++ musmtl/tool.py
@@ -20,7 +20,7 @@ from .utils import extract_mel
 from .config import Config as cfg
 
 
-@background(max_prefetch=5)
+# @background(max_prefetch=10)
 def _generate_mels(fns):
     for fn in tqdm(fns, ncols=80):
         try:
@@ -34,77 +34,109 @@ def _generate_mels(fns):
             print(e)
         else:
             yield fn, y
-        
+
 
 class FeatureExtractor(object):
-    def __init__(self, model_checkpoint, target_audios, is_gpu=False):
+    def __init__(self, target_audios, is_gpu=False):
         """
         Args:
-            model_checkpoint (str): path to model checkpoint file (.pth.gz)
             target_audios (str): path to file listing target audio files (.txt)
         """
         # build & initialize model
         self.is_gpu = is_gpu
-        
+
+        # get the target melspec list
+        with open(target_audios, 'r') as f:
+            self.mel_fns = [ll.replace('\n', '') for ll in f.readlines()]
+
+    @staticmethod
+    def _load_model(fn, is_gpu):
+        """"""
         # load checkpoint to the model
         checkpoint = torch.load(
-            model_checkpoint, map_location=lambda storage, loc: storage)
-        self.model = VGGlikeMTL(checkpoint['tasks'],
-                                checkpoint['branch_at'])
-        self.model.load_state_dict(checkpoint['state_dict'])
-        self.model.eval()
-        
+            fn, map_location=lambda storage, loc: storage)
+        model = VGGlikeMTL(checkpoint['tasks'],
+                           checkpoint['branch_at'])
+        model.load_state_dict(checkpoint['state_dict'])
+        model.eval()
+
         # initialize scaler
         sclr_ = joblib.load(checkpoint['scaler_fn'])
-        self.scaler = SpecStandardScaler(sclr_.mean_, sclr_.scale_)
-        
-        if self.is_gpu:
-            self.scaler.cuda()
-            self.model.cuda()
-    
-        # get the target list
-        with open(target_audios, 'r') as f:
-            self.fns = [ll.replace('\n', '') for ll in f.readlines()]
-        
-    def run(self):
+        scaler = SpecStandardScaler(sclr_.mean_, sclr_.scale_)
+
+        if is_gpu:
+            scaler.cuda()
+            model.cuda()
+
+        return scaler, model
+
+    @staticmethod
+    def _preprocess_mel(y, is_gpu):
         """"""
-        output = []
-        for fn, y in _generate_mels(self.fns):
-
-            # zero padding
-            if y.shape[1] % cfg.N_STEPS != 0:
-                margin = cfg.N_STEPS - y.shape[1] % cfg.N_STEPS
-                y = np.concatenate(
-                    [y, np.zeros((cfg.N_CH, margin, cfg.N_BINS))],
-                    axis=1
-                ).astype(np.float32)  # (2, t, 128)
-
-            # re-organize it to have (N, 2, 216, 128)
-            Y = np.concatenate(
-                [y[j].reshape(-1, cfg.N_STEPS, cfg.N_BINS)[:, None]
-                 for j in range(cfg.N_CH)],
+        # zero padding
+        if y.shape[1] % cfg.N_STEPS != 0:
+            margin = cfg.N_STEPS - y.shape[1] % cfg.N_STEPS
+            y = np.concatenate(
+                [y, np.zeros((cfg.N_CH, margin, cfg.N_BINS))],
                 axis=1
-            )
-            Y = Variable(torch.from_numpy(Y).float())
-            
-            if self.is_gpu:
-                Y = Y.cuda()
-            
-            # scaling & extraction
-            Y = self.scaler(Y)
-            Y = torch.cat(
-                [self.model.feature(Y, task)
-                 for task in self.model.tasks],
-                dim=1
-            )
-            
-            if self.is_gpu:
-                Y = Y.data.cpu().numpy()
-            else:
-                Y = Y.data.numpy()
-            
-            # concat of mean / std
-            z = np.r_[Y.mean(axis=0), Y.std(axis=0)]
-            output.append(z)
-            
-        return np.array(output)  # (n x (d x m))
+            ).astype(np.float32)  # (2, t, 128)
+
+        # re-organize it to have (N, 2, 216, 128)
+        Y = np.concatenate(
+            [y[j].reshape(-1, cfg.N_STEPS, cfg.N_BINS)[:, None]
+             for j in range(cfg.N_CH)],
+            axis=1
+        )
+        Y = Variable(torch.from_numpy(Y).float())
+
+        if is_gpu:
+            Y = Y.cuda()
+
+        return Y
+
+    def _extract(self, scaler, model, Y):
+        # scaling & extraction
+        Y = scaler(Y)
+        Y = torch.cat(
+            [model.feature(Y, task) for task in model.tasks],
+            dim=1
+        )
+
+        if self.is_gpu:
+            Y = Y.data.cpu().numpy()
+        else:
+            Y = Y.data.numpy()
+
+        # concat of mean / std
+        z = np.r_[Y.mean(axis=0), Y.std(axis=0)]
+        return z
+
+    def run(self, model_fns):
+        """
+        Args:
+            model_fns (list of str): list of path to model checkpoint file (.pth.gz)
+        """
+        # load all melspecs
+        # TODO: this approach is memory heavy, but fast. better way?
+        X = list(_generate_mels(self.mel_fns))
+
+        for model_fn in model_fns:
+
+            # initiate output containor
+            output = []
+
+            # spawn model
+            scaler, model = self._load_model(model_fn, self.is_gpu)
+
+            # process
+            for fn, x in X:
+                Y = self._preprocess_mel(x, self.is_gpu)
+                z = self._extract(scaler, model, Y)
+                output.append(z)
+
+            yield np.array(output)  # (n x (d x m))
+
+        # # OLD WAY
+        # for fn, y in _generate_mels(self.fns):
+        #     output.append(z)
+        # return np.array(output)  # (n x (d x m))
--- scripts/ext_feat.py
+++ scripts/ext_feat.py
@@ -1,4 +1,5 @@
 import os
+from os.path import dirname, basename, join, exists
 import sys
 # add repo root to the system path
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
@@ -11,20 +12,38 @@ from musmtl.utils import extract_mel, parmap
 
 # setup arg parser
 parser = argparse.ArgumentParser()
-parser.add_argument("model_checkpoint", help='path to model checkpoint file (.pth.gz)')
+parser.add_argument("model_checkpoints", help='path to file listing model checkpoint files (.txt)')
 parser.add_argument("target_audios", help='path to file listing target audio files (.txt)')
-parser.add_argument("out_fn", help='filename for the output')
+parser.add_argument("out_root", help='filepath for the output')
 parser.add_argument("--is-gpu", type=bool, default=False,
                     help='flag for gpu computation on feature extraction')
 args = parser.parse_args()
 
+# parse model paths
+model_fns = []
+with open(args.model_checkpoints) as f:
+    for line in f:
+        fn = line.replace('\n', '')
+
+        # check input / output path is existing and valid
+        if not exists(fn):
+            raise IOError('[ERROR] model checkpoint not exists!')
+
+        model_fns.append(fn)
+
 # init worker and run!
 print('Initiating worker...')
-ext = FeatureExtractor(
-    args.model_checkpoint, args.target_audios,
-    args.is_gpu
-)
-feature = ext.run()
+ext = FeatureExtractor(args.target_audios, args.is_gpu)
 
-print('Saving...')
-np.save(args.out_fn, feature)
+print('Processing...')
+for feature, fn in zip(ext.run(model_fns), model_fns):
+
+    # get exp id
+    ix = basename(fn).split('_')[0].split('.')[-1]
+    out_fn = join(args.out_root, ix,
+                  basename(args.target_audios).split('_')[0] + '.npy')
+    if not exists(dirname(out_fn)):
+        os.makedirs(dirname(out_fn))
+
+    print('Saving {}...'.format(ix))
+    np.save(out_fn, feature)
